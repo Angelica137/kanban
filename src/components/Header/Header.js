78d477891dc93d7c651e4b1df11f618e86be0d44
Angelica <Angelica137@users.noreply.github.com>
2020-09-20 15:04:49 +0100

    add header wrapper

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
-const Header = () => <h1>This is your Kanban</h1>;
+const HeaderWrapper = styled.div`
+  background-color: #23ce6b;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Header = () => (
+  <HeaderWrapper>
+    <h1>This is your Kanban</h1>
+  </HeaderWrapper>
+);
 
 export default Header;
