db40669d42e2ce6299f6770159c17da61cd7cdd5
Angelica <Angelica137@users.noreply.github.com>
2020-09-20 15:22:07 +0100

    add column component to app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import Header from "../components/Header/Header";
+import Column from "../components/Column/Column";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <Header />;
+    return (
+      <>
+        <Header />
+        <Column />
+        <Column />
+      </>
+    );
   }
 }
 
