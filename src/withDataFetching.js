afa942594e39312ec3e4fec66b305f8954210b64
Angelica <Angelica137@users.noreply.github.com>
2020-09-21 00:45:03 +0100

    add constructor to withData

--- src/withDataFetching.js
+++ src/withDataFetching.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function witDataFetching(WrappedComponent) {
-  return class extends React.Component {};
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: [],
+        loading: true,
+        error: "",
+      };
+    }
+  };
 }
