ce1e05cca7f8591fff6b57ed849fba82ad64df7f
Angelica <Angelica137@users.noreply.github.com>
2020-09-20 21:55:42 +0100

    add data to board

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -14,7 +14,35 @@ const BoardWrapper = styled.div`
 `;
 
 class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      loading: true,
+      error: "",
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const tickets = await fetch("../..assets/data.json");
+      const ticketsJSON = await tickets.json();
+
+      if (ticketsJSON) {
+        this.setState({
+          data: ticketsJSON,
+          loading: false,
+        });
+      }
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error.message,
+      });
+    }
+  }
   render() {
+    const { data, loading, error } = this.state;
     const columns = [
       { id: 1, title: "To Do" },
       { id: 2, title: "In Progress" },
@@ -25,7 +53,13 @@ class Board extends Component {
     return (
       <BoardWrapper>
         {columns.map((column) => (
-          <Column key={column.id} title={column.title} />
+          <Column
+            key={column.id}
+            title={column.title}
+            loading={loading}
+            error={error}
+            tickets={data.filter((ticket) => ticket.column === column.id)}
+          />
         ))}
       </BoardWrapper>
     );
